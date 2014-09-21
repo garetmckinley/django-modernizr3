9fefab33b67770fbedea26d79891ee2d2fd6532b
Garet McKinley <garetmckinley@me.com>
2014-09-21 19:36:30 -0400

    Fixed python 3 django imports

--- modernizr/middleware.py
+++ modernizr/middleware.py
@@ -9,7 +9,7 @@ https://github.com/robhudson/django-debug-toolbar
 from django.conf import settings
 from django.http import HttpResponse, QueryDict
 from django.template.loader import render_to_string
-from django.utils.encoding import smart_unicode
+from django.utils.encoding import smart_text
 from django.utils.http import cookie_date
 
 from modernizr.settings import contribute_to_settings
@@ -18,7 +18,7 @@ from modernizr.settings import contribute_to_settings
 contribute_to_settings({
     'MODERNIZR_STORAGE': 'cookie',
     'MODERNIZR_COOKIE_NAME': 'modernizr',
-    'MODERNIZR_COOKIE_AGE': 60 * 60 * 24 * 7 * 2, # 2 weeks
+    'MODERNIZR_COOKIE_AGE': 60 * 60 * 24 * 7 * 2,  # 2 weeks
     'MODERNIZR_COOKIE_DOMAIN': None,
     'MODERNIZR_COOKIE_SECURE': False,
     'MODERNIZR_COOKIE_PATH': '/',
@@ -30,6 +30,7 @@ contribute_to_settings({
 
 _HTML_TYPES = ('text/html', 'application/xhtml+xml')
 
+
 def replace_insensitive(string, target, replacement):
     """
     Similar to string.replace() but is case insensitive
@@ -39,8 +40,9 @@ def replace_insensitive(string, target, replacement):
     index = no_case.rfind(target.lower())
     if index >= 0:
         return string[:index] + replacement + string[index + len(target):]
-    else: # no results so return the original string
+    else:  # no results so return the original string
         return string
+
 
 class ModernizrMiddleware(object):
 
@@ -48,12 +50,13 @@ class ModernizrMiddleware(object):
         data = request.GET
         if settings.MODERNIZR_STORAGE == 'cookie':
             response.set_cookie(settings.MODERNIZR_COOKIE_NAME,
-                data.urlencode(),
-                max_age=settings.MODERNIZR_COOKIE_AGE,
-                expires=cookie_date(settings.MODERNIZR_COOKIE_AGE),
-                domain=settings.MODERNIZR_COOKIE_DOMAIN,
-                path=settings.MODERNIZR_COOKIE_PATH,
-                secure=settings.MODERNIZR_COOKIE_DOMAIN or None)
+                                data.urlencode(),
+                                max_age=settings.MODERNIZR_COOKIE_AGE,
+                                expires=cookie_date(
+                                settings.MODERNIZR_COOKIE_AGE),
+                                domain=settings.MODERNIZR_COOKIE_DOMAIN,
+                                path=settings.MODERNIZR_COOKIE_PATH,
+                                secure=settings.MODERNIZR_COOKIE_DOMAIN or None)
         elif settings.MODERNIZR_STORAGE == 'session':
             request.session[settings.MODERNIZR_SESSION_KEY] = data
         else:
@@ -64,12 +67,14 @@ class ModernizrMiddleware(object):
         data = None
         if settings.MODERNIZR_STORAGE == 'cookie':
             if settings.MODERNIZR_COOKIE_NAME in request.COOKIES:
-                data = QueryDict(request.COOKIES[settings.MODERNIZR_COOKIE_NAME])
+                data = QueryDict(
+                    request.COOKIES[settings.MODERNIZR_COOKIE_NAME])
         elif settings.MODERNIZR_STORAGE == 'session':
             data = request.session.get(settings.MODERNIZR_SESSION_KEY)
 
         if data is not None:
-            request.modernizr = dict([(k, bool(int(v))) for k,v in data.items()])
+            request.modernizr = dict(
+                [(k, bool(int(v))) for k, v in data.items()])
         else:
             request.modernizr = None
 
@@ -80,9 +85,9 @@ class ModernizrMiddleware(object):
         })
         tag = u'</' + settings.MODERNIZR_INCLUDE_TAG + u'>'
         response.content = replace_insensitive(
-            smart_unicode(response.content), 
+            smart_text(response.content),
             tag,
-            smart_unicode(modernizr_content + tag))
+            smart_text(modernizr_content + tag))
         if response.get('Content-Length', None):
             response['Content-Length'] = len(response.content)
         return response

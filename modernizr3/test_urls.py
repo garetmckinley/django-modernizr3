59b7ab396cfeb9d73571c2069693395d5f8b1cda
mediachicken <garetmckinley@me.com>
2014-09-21 21:57:00 -0400

    PEP8 and changed import

--- modernizr3/test_urls.py
+++ modernizr3/test_urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls.defaults import *
 
-from modernizr import test_views
+from modernizr3 import test_views
 
 urlpatterns = patterns('',
-    url(r'^$', test_views.test_view),
-    url(r'^content-length/$', test_views.test_view_with_content_length),
-    url(r'^charset/$', test_views.test_view_with_charset),
-    url(r'^404/$', test_views.test_view_404),
-    url(r'^css/$', test_views.test_css),
-    url(r'^tag/$', test_views.test_tag),
-    url(r'^no-tag/$', test_views.test_no_tag),
-)
+                       url(r'^$', test_views.test_view),
+                       url(r'^content-length/$',
+                           test_views.test_view_with_content_length),
+                       url(r'^charset/$', test_views.test_view_with_charset),
+                       url(r'^404/$', test_views.test_view_404),
+                       url(r'^css/$', test_views.test_css),
+                       url(r'^tag/$', test_views.test_tag),
+                       url(r'^no-tag/$', test_views.test_no_tag),
+                       )
